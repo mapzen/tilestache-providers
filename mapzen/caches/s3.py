2d74261c06444f2e7fa69f79c6cff733415eb10b
Robert Marianski <rob@marianski.com>
2014-09-11 16:12:00 -0400

    support gzipping formats in s3

--- mapzen/caches/s3.py
+++ mapzen/caches/s3.py
@@ -1,4 +1,5 @@
 from cStringIO import StringIO
+from gzip import GzipFile
 from mapzen.mimetypes import lookup_mimetype_from_format_name
 from TileStache.Core import TheTileLeftANote
 from TileStache.S3 import Cache as TilestacheS3Cache
@@ -7,12 +8,15 @@ from wsgiref.headers import Headers
 
 class S3(object):
 
-    def __init__(self, bucket=None, access=None, secret=None, use_locks=True, path='', reduced_redundancy=False):
+    def __init__(self, bucket=None, access=None, secret=None, use_locks=True, path='', reduced_redundancy=False, gzip_formats=None):
         assert bucket is not None
         assert access is not None
         assert secret is not None
 
         self.tilestache_s3_cache = TilestacheS3Cache(bucket, access, secret, use_locks, path, reduced_redundancy)
+        if gzip_formats is None:
+            gzip_formats = ()
+        self.gzip_formats = set(gzip_formats)
 
     def lock(self, layer, coord, format):
         if not self.tilestache_s3_cache.use_locks:
@@ -36,7 +40,13 @@ class S3(object):
 
         out = StringIO()
         key.get_file(out)
-        content = out.getvalue()
+
+        if format.lower() in self.gzip_formats:
+            out.seek(0)
+            with GzipFile(mode='rb', fileobj=out) as gz_stream:
+                content = gz_stream.read()
+        else:
+            content = out.getvalue()
 
         mimetype = lookup_mimetype_from_format_name(format) or \
                 'application/octet-stream'
@@ -52,12 +62,17 @@ class S3(object):
         key_name = tile_key(layer, coord, format, self.tilestache_s3_cache.path)
         key = self.tilestache_s3_cache.bucket.new_key(key_name)
 
-        mimetype = lookup_mimetype_from_format_name(format) or \
-                'application/octet-stream'
-        headers = {'Content-Type': mimetype}
-
         out = StringIO()
-        out.write(body)
+        if format.lower() in self.gzip_formats:
+            mimetype = 'application/x-gzip'
+            with GzipFile(mode='wb', fileobj=out, compresslevel=9) as gzip_out:
+                gzip_out.write(body)
+        else:
+            mimetype = lookup_mimetype_from_format_name(format) or \
+                    'application/octet-stream'
+            out.write(body)
+
+        headers = {'Content-Type': mimetype}
         out.seek(0)
 
         key.set_contents_from_file(
